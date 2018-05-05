dfbe91735e81b15b836703dae5497c9581437fd0
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-05-05 13:08:15 +0300

    sign up page

--- src/Routes.js
+++ src/Routes.js
@@ -5,11 +5,13 @@ import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 export default ({ childProps }) => (
 	<Switch>
 		<AppliedRoute path="/" exact component={Home} props={childProps} />
 		<AppliedRoute path="/login" exact component={Login} props={childProps} />
+		<AppliedRoute path="/signup" exact component={Signup} props={childProps} />
 		{/* Finally, catch all unmatched routes */}
 		<Route component={NotFound} />
 	</Switch>

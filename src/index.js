74bd4d73598b96a383d93b82f18ff2e272e7d5b5
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-05-05 12:03:53 +0300

    Add amplify sdk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import config from './config';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
+Amplify.configure({
+	Auth: {
+		mandatorySignIn: true,
+		region: config.cognito.REGION,
+		userPoolId: config.cognito.USER_POOL_ID,
+		identityPoolId: config.cognito.IDENTITY_POOL_ID,
+		userPoolWebClientId: config.cognito.APP_CLIENT_ID
+	},
+	API: {
+		endpoints: [
+			{
+				name: 'testApi',
+				endpoint: config.apiGateway.URL,
+				region: config.apiGateway.REGION
+			}
+		]
+	}
+});
 
 ReactDOM.render(
 	<Router>
